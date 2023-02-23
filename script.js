98dc6ed0d8c9ead0f1319bad84ee370af0077ec6
unknown <wesley_luiz2001@hotmail.com>
2023-02-23 16:09:13 +0000

    added content and the api to make it work

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ const button = document.getElementById('btn');
 
 async function selectMediaStream() {
     try {
-
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        VideoElement.srcObject = mediaStream;
+        VideoElement.onloadedmetadata = () => {
+            VideoElement.play();
+        }
     } catch (error) {
         // Catch error Here
         console.log('error sorry buddy', error);
     }
 }
+
+// On load
+selectMediaStream();
