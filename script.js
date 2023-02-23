75ffd3169a41103363d2c29b975679d021cbd524
unknown <wesley_luiz2001@hotmail.com>
2023-02-23 15:58:59 +0000

    started working on the script file, added video,button and async function

---
+++ script.js
@@ -0,0 +1,13 @@
+const VideoElement = document.getElementById('video')
+const button = document.getElementById('btn');
+
+// async function, prompt to select media stream, pass video element and then play
+
+async function selectMediaStream() {
+    try {
+
+    } catch (error) {
+        // Catch error Here
+        console.log('error sorry buddy', error);
+    }
+}
